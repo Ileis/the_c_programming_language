3e03076a2064b405d9dc79c52183e6f92ea81dc5
Ileis <gabrielileis07@gmail.com>
2025-10-26 18:22:25 -0300

    exercise 2-5

---
+++ chap02/ex02/02_05.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int any(char s1[], char s2[]) {
+    int i, j;
+    for (i = 0; s1[i] != '\0'; ++i)
+        for (j = 0; s2[j] != '\0'; ++j)
+            if (s1[i] == s2[j]) return i;
+    return -1;
+}
+
+int main() {
+    char s1[] = "gabriel";
+    char s2[] = "aeiou";
+    printf("first occurrence: %d\n", any(s1, s2));
+
+    return EXIT_SUCCESS;
+}
