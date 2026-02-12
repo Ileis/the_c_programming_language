1b845bc905f3e8e5fde8f97f6a2f2147958361e8
Ileis <gabrielileis07@gmail.com>
2026-02-12 18:46:36 -0300

    exercise 4-1

---
+++ chap04/01_simplified_grep.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAXLINE 1000
+
+int get_line(char line[], int max);
+int strindex(char source[], char searchfor[]);
+
+int main() {
+    char pattern[] = "ould";
+    char line[MAXLINE];
+    int found = 0;
+    int index;
+    while (get_line(line, MAXLINE) > 0) {
+        if ((index = strindex(line, pattern)) >= 0) {
+            printf("%s", line);
+            printf("index: %d\n", index);
+            found++;
+        }
+    }
+    return found;
+}
+
+int get_line(char line[], int max) {
+    int c;
+    int i = 0;
+    while (--max > 0 && (c = getchar()) != EOF && c != '\n') line[i++] = c;
+    if (c == '\n') line[i++] = c;
+    line[i] = '\0';
+    return i;
+}
+
+int strindex(char source[], char searchfor[]) {
+    int i, j, k;
+    for (i = 0; source[i] != '\0'; ++i) {
+        for (j = i, k = 0; searchfor[k] != '\0' && source[j] == searchfor[k]; ++j, ++k);
+        if (k > 0 && searchfor[k] == '\0') return i;
+    }
+    return -1;
+}
